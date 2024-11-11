62e25c0a7a1edd48d15bf5116b8f10aecdf553f3
Ahmed Shariff <shariff.mfa@outlook.com>
2024-11-11 04:19:20 -0800

    Fix for path base

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
  import "../app.css";
+ import { base } from '$app/paths'
  import IoCevronUpCircleArrow from "$lib/icons/ioCevronUpCircleArrow.svelte";
  let { children } = $props();
 </script>
@@ -12,7 +13,7 @@
 <div class='flex flex-col min-h-screen bg-blue-100'>
     <header class='bg-blue-800 mb-0 md:mb-8 py-1 text-gray-200 md:sticky top-0 left-0 right-0 drop-shadow-lg shadow-blue-900 z-10'>
         <div class='container mx-auto flex flex-col md:flex-row gap-x-12 items-center justify-center'>
-            <a class="nav-btn font-semibold text-lg" href="/">HCI deadlines</a>
+            <a class="nav-btn font-semibold text-lg" href={base + '/'}>HCI deadlines</a>
             <a class="nav-btn" href="https://ovi.ok.ubc.ca">OVI lab</a>
         </div >
     </header >
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const path_base = process.argv.includes('dev') ? '' : process.env.BASE_PATH;
+
+console.log("using path.base (", process.argv.includes('dev'), ")", path_base);
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -16,7 +20,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: path_base
 		}
 	}
 };
